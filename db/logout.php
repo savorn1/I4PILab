355b9edbfff308c3e6aa53f6a04f8fa633635bc2
HENG Sotharith <heng.sotharith@gmail.com>
2017-05-12 16:32:18 +0700

    update structure

--- db/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-	session_start();
-	if(isset($_SESSION['username'])){
-		setcookie('username', '', time()-3600);
-		session_destroy();
-	}
-
-	header('Location: login.php');
-?>
--- db/salt.php
+++
@@ -1,3 +0,0 @@
-<?php
-	define('KEY_SALT', '12323jk2j3kjkjkj');
-?>
